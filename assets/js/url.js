284531604b19bd1028ef79830f2cc77bdbf4f61b
Ansh Sharma <ansharma013@gmail.com>
2025-04-10 23:19:32 +0530

    updated

--- assets/js/url.js
+++ assets/js/url.js
@@ -4,9 +4,8 @@ const REGISTER_URL = BASE_URL + "/api/users/register";
 const BOOK_URL = BASE_URL + "/api/books/all";
 const ADD_BOOK_URL = BASE_URL + "/api/books/add";
 const DELETE_BOOK_URL = BASE_URL + "/api/books/delete";
-const CREATE_CART_URL = BASE_URL + "/api/carts/create";
 const CHECKOUT_URL = BASE_URL + "/api/carts/checkout";
 const ADD_TO_CART_URL = BASE_URL + "/api/carts/add-to-cart";
-const GET_ALL_CART_URL = BASE_URL + "/api/carts/get-all-cart";
 const GET_CURRENT_CART_URL = BASE_URL + "/api/carts/current";
-export { BASE_URL, LOGIN_URL, REGISTER_URL, BOOK_URL, ADD_BOOK_URL, DELETE_BOOK_URL, CREATE_CART_URL, CHECKOUT_URL, ADD_TO_CART_URL, GET_ALL_CART_URL ,GET_CURRENT_CART_URL};
+const CLEAR_CART_URL = BASE_URL + "/api/carts/delete"
+export { BASE_URL, LOGIN_URL, REGISTER_URL, BOOK_URL, ADD_BOOK_URL, DELETE_BOOK_URL, CHECKOUT_URL, ADD_TO_CART_URL ,GET_CURRENT_CART_URL, CLEAR_CART_URL};
